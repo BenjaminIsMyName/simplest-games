cf89d869559c20e2881b918af8422ea74dc0442c
Benjamin <87870889+BenjaminIsMyName@users.noreply.github.com>
2023-07-11 19:59:19 +0300

    Added comments in car game

--- car/index.js
+++ car/index.js
@@ -1,7 +1,7 @@
-let curr = -50;
+let curr = -50
 
 function handleMove(num) {
-  let ourCar = document.getElementById("car");
-  ourCar.style.transform = `rotate(${num}deg) translate(${curr + num}%)`;
-  curr = curr + num;
+    let ourCar = document.getElementById('car')
+    ourCar.style.transform = `rotate(${num}deg) translate(${curr + num}%)`
+    curr = curr + num
 }
