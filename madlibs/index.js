675808ca4ef60c21a605648c369562ee9debea53
Benjamin <87870889+BenjaminIsMyName@users.noreply.github.com>
2023-07-20 20:53:28 +0300

    Added mad libs game

---
+++ madlibs/index.js
@@ -0,0 +1,19 @@
+let actor = document.getElementById('actor')
+let food = document.getElementById('food')
+let animal = document.getElementById('animal')
+let adjective = document.getElementById('adjective')
+
+let result = document.getElementById('result')
+
+function create() {
+    let stories = [
+        `לפני שנים רבות היה ילד בשם ${actor.value} שחזר מהגן וראה על הושלחן בבית ${food.value} ואז הילד נגעל ורצה להקיא. הוא הקיא על חיית המחמד שלו, ${animal.value} והחיה הייתה מאוד ${adjective.value}`,
+        `בגרמניה של שנות ה-30 השחקן ${actor.value} היה מאוד ${adjective.value}. כשהוא היה קטן חשבו שהוא דומה ל${animal.value}. `,
+    ]
+    let num = randomInteger(0, stories.length - 1)
+    result.innerHTML = stories[num]
+}
+
+function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
