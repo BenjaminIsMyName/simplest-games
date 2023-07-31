cc734328f5f2f82734d596851e23ad42e7b619ad
Benjamin <87870889+BenjaminIsMyName@users.noreply.github.com>
2023-07-31 15:47:29 +0300

    Simplified arrows game

--- arrows/index.js
+++ arrows/index.js
@@ -20,14 +20,9 @@ let picked = 0
 setInterval(pickImage, 3000)
 
 function pickImage() {
-    arrowElement.classList.remove('animate')
-    arrowElement.offsetHeight
     picked = randomInteger(0, 3)
     arrowElement.setAttribute('src', arrows[picked])
-    arrowElement.classList.add('animate')
 }
-
-pickImage()
 
 function randomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
