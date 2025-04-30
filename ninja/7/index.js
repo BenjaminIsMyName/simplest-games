cf6378b636f8851a28986d97eb2b90e31ba426d3
Benjamin <87870889+BenjaminIsMyName@users.noreply.github.com>
2025-04-30 17:01:48 +0300

    Better game

--- ninja/7/index.js
+++ ninja/7/index.js
@@ -1,28 +1,28 @@
-function randomInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min
-}
-
-let time = 5
+let time = 10
 let score = 0
-
+let moveBoxesInterval = Math.max(500, 3000 - score * 100)
 let randomNumber = document.getElementById('randomNumber')
 let scoreElement = document.getElementById('score')
 let picked = undefined
-let interval = undefined
+let timerInterval = undefined
 let timerId = document.getElementById('timerId')
 let timeLeft = time
+let lastMoveTime = Date.now()
+
+let buttons = document.querySelectorAll('button')
 
 pick()
+moveBoxes()
 
 function check(clicked) {
     if (picked == clicked) {
         score = score + 1
         pick()
+        adjustMoveBoxesSpeed()
     } else {
         alert('You lost because you clicked wrong! Your score is: ' + score)
         window.location.reload()
     }
-
     scoreElement.innerHTML = 'Score: ' + score
 }
 
@@ -30,27 +30,12 @@ function pick() {
     scoreElement.innerHTML = 'Score: ' + score
     picked = randomInteger(1, 3)
     randomNumber.innerHTML = picked
-    if (interval) {
-        clearInterval(interval)
+    if (timerInterval) {
+        clearInterval(timerInterval)
     }
-    interval = setInterval(timer, 1000)
+    timerInterval = setInterval(timer, 1000)
     timeLeft = time
     timerId.innerHTML = 'Time left: ' + timeLeft
-
-    // Increase the speed of the buttons every 5 points
-    // 5: -1
-    // 10: -2
-    // 15: -3
-    if (score % 5 == 0 && score != 0 && score < 20) {
-        let btn1 = document.getElementById('btn1')
-        let btn2 = document.getElementById('btn2')
-        let btn3 = document.getElementById('btn3')
-
-        // TODO: currently, when changing the duration - the animation is resetting and flickering. Fix it.
-        btn1.style.animationDuration = 5 - score / 5 + 's'
-        btn2.style.animationDuration = 5 - score / 5 + 's'
-        btn3.style.animationDuration = 5 - score / 5 + 's'
-    }
 }
 
 function timer() {
@@ -61,4 +46,38 @@ function timer() {
         alert('You lost because of time! Your score is: ' + score)
         window.location.reload()
     }
+}
+
+function moveBox(box) {
+    const x = randomInteger(0, window.innerWidth - 50)
+    const y = randomInteger(0, window.innerWidth - 50)
+    box.style.left = `${x}px`
+    box.style.top = `${y}px`
+}
+
+function moveBoxes() {
+    const now = Date.now()
+    if (now - lastMoveTime >= moveBoxesInterval) {
+        for (const button of buttons) {
+            button.style.position = 'absolute'
+            moveBox(button)
+        }
+        lastMoveTime = now
+    }
+    requestAnimationFrame(moveBoxes)
+}
+
+// Move boxes immediately when the game starts
+for (const button of buttons) {
+    button.style.position = 'absolute'
+    moveBox(button)
+}
+requestAnimationFrame(moveBoxes)
+
+function randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function adjustMoveBoxesSpeed() {
+    moveBoxesInterval = Math.max(500, 3000 - score * 100) // Minimum interval of 500ms
 }
