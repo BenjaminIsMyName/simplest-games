d7ee56616c76df65a3497dfe93a9419c0e08a280
Benjamin <87870889+BenjaminIsMyName@users.noreply.github.com>
2025-12-10 15:28:28 +0200

    FULL spaceship game

--- python/project-spaceship-game/main.py
+++ python/project-spaceship-game/main.py
@@ -1,4 +1,5 @@
 from pygame import * # 1
+import random # 49
 
 init() # 2
 
@@ -7,19 +8,48 @@ screen = display.set_mode((800, 600)) # 3
 display.set_caption("Space War") # 10
 
 # Background
-background = image.load('23/background.png') # 11
+background = image.load('27/background.png') # 11
+
+# Background sound
+mixer.music.load("27/background.wav") # 80
+mixer.music.play() # 81 (for loop, pass -1 to the play() function)
 
 # Player
-player_surf = image.load('23/player.png') # 13
+player_surf = image.load('27/player.png') # 13
 player_rect = Rect(370, 480, player_surf.get_width(), player_surf.get_height()) # 14
 
+# Enemies
+enemy_imgs = [] # 41 <<<<<
+enemy_rects = [] # 42
+# Each one has its own direction, the movement for each can be 24 or -24
+enemyX_change = [] # 43
+# Everyone goes down 40, nothing goes up
+enemyY_change = 40 # 44
+num_of_enemies = 6 # 45
+
+for i in range(num_of_enemies): # 46
+    enemy_imgs.append(image.load('27/enemy.png')) # 47
+    one_rect = Rect(random.randint(0, 736), random.randint(50, 150), # 48 (line 49 is the import)
+                           enemy_imgs[-1].get_width(), enemy_imgs[-1].get_height()) # 50
+    enemy_rects.append(one_rect) # 51
+    enemyX_change.append(24) # 52
+    pass # 53 (no need)
+
+
 # Bullet
-bullet_surf = image.load('23/bullet.png') # 27
+bullet_surf = image.load('27/bullet.png') # 27  
 bullet_rect = Rect(0, player_rect.y, bullet_surf.get_width(), bullet_surf.get_height()) # 28
 bullet_state = "ready" # 29
 
-clock = time.Clock() # 25
 
+# Fonts
+over_font = font.SysFont("Arial", 64) # 67
+score_font = font.SysFont("Arial", 32) # 75
+
+
+score_value = 0 # 76
+
+clock = time.Clock() # 25
 
 running = True # 4
 
@@ -42,6 +72,8 @@ while running: # 5
                     bullet_rect.x = player_rect.x # 33
                     # Start firing 
                     bullet_state = "fire" # 34
+                    bullet_sound = mixer.Sound("27/laser.wav") # 82
+                    bullet_sound.play() # 83
 
     keys_pressed = key.get_pressed() # 16
     if keys_pressed[K_LEFT]: # 17
@@ -53,6 +85,40 @@ while running: # 5
         player_rect.x = 0 # 22
     elif player_rect.x >= 736: # 23
         player_rect.x = 736 # 24
+
+    # Handle the enemies
+    for i in range(num_of_enemies): # 54
+        
+        screen.blit(enemy_imgs[i], enemy_rects[i]) # 55
+
+        # Move the enemies
+        enemy_rects[i].x += enemyX_change[i] # 56
+        if enemy_rects[i].x > 736: # 57    
+            enemyX_change[i] = -24 # 58    
+            enemy_rects[i].y += enemyY_change # 59   
+        elif enemy_rects[i].x < 0: # 60    
+            enemyX_change[i] = 24 # 61   
+            enemy_rects[i].y += enemyY_change # 62   
+
+        # Game Over
+        if enemy_rects[i].y > 440: # 63
+            for j in range(num_of_enemies): # 64
+                enemy_rects[j].y = 2000 # 65
+            over_text = over_font.render("GAME OVER", True, (255, 255, 255)) # 66 (line 67 is to create the font)
+            screen.blit(over_text, (200, 250)) # 68
+            # Break out of the outer loop (that handles the enemies)
+            break # 69
+
+        # Collision - the bullet fired and hit the enemy
+        if enemy_rects[i].colliderect(bullet_rect) and bullet_state == "fire": # 70
+            bullet_rect.y = player_rect.y # 71
+            bullet_state = "ready" # 72
+            # It's dead, so return the enemy to the starting point
+            enemy_rects[i].x = random.randint(0, 736) # 73
+            enemy_rects[i].y = random.randint(50, 150) # 74
+            score_value += 1 # 79 (lines 75-79 are all for the score)
+            explosion_sound = mixer.Sound("27/explosion.wav") # 84
+            explosion_sound.play() # 85
 
     
     # If the bullet is far up, don't "blit" it anymore
@@ -66,6 +132,9 @@ while running: # 5
         bullet_rect.y -= 40 # 37
     
     screen.blit(player_surf, player_rect) # 15
+
+    score = score_font.render("Score : " + str(score_value), True, "white") # 77
+    screen.blit(score, (10, 10)) # 78
 
     clock.tick(40) # 26
     display.update() # 9
